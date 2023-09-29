487b1741c61b66e23193c2483a0862fe13f25547
VPolishchuk <vpolishchuk76@gmail.com>
2023-09-29 14:01:25 +0300

    add pwa beta

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,50 @@
+// //@ts-check
+
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const { composePlugins, withNx } = require('@nx/next');
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+// });
+// /**
+//  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+//  * @type {import('next').NextConfig}
+//  **/
+// const nextConfig = {
+//   nx: {
+//     // Set this to true if you would like to use SVGR
+//     // See: https://github.com/gregberge/svgr
+//     svgr: false,
+//   },
+// };
+
+// const plugins = [
+//   // Add more Next.js plugins to this list if needed.
+//   withNx,
+// ];
+
+// module.exports = composePlugins(...plugins)(nextConfig);
+
+
+// // /** @type {import('next').NextConfig} */
+
+
+
+// // // module.exports = withPWA({
+// // //   reactStrictMode: true,
+// // // });
 //@ts-check
+//  * @type {import('next').NextConfig}
+
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -17,6 +60,7 @@ const nextConfig = {
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+  withPWA,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
